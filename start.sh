7b5631d7ecd78a41ff6bc5a94e0336ea97b9e335
Energy1190 <energyneo0@gmail.com>
2017-08-28 14:08:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,23 +4,29 @@ HOST_NAME=$(hostname)
 PASSWD=${HACLUSTER}
 HOSTS=()
 HOSTS+=("${HOST_NAME}")
+MAX_NODES=$(curl -s http://${ETCD_HOST}:2379/v2/keys/mysql/max_nodes | jq -r '.node.value')
 
 for node in $(curl -s http://${ETCD_HOST}:2379/v2/keys/mysql/ip/ | jq -r '.node.nodes[] | .key + "=" + .value'); do
 IFS='=' read -ra RESULT <<< $node
 IP=${RESULT[-1]}
 IFS='/' read -ra KEY_MAP <<< ${RESULT[0]}
 HOST=${KEY_MAP[-1]}
-if [ "${HOST}" != "mysql-${HOST_NAME}" ]; then
 echo "${me} => Get node ip/name - ${IP}/${HOST}"
 echo "${IP}    ${HOST}" | sed 's/mysql-//g' >> /etc/hosts
 H=$(echo "${HOST}" | sed 's/mysql-//g')
 HOSTS+=("${H}")
-fi
 done
 
-echo "hacluster:${HACLUSTER}" | chgpasswd
+echo "hacluster:${HACLUSTER}" | chpasswd
+service pcsd start
+sleep 10
+service pcsd status
+curl -s http://${ETCD_HOST}:2379/v2/keys/mysql/pcs/${HOST_NAME} -XPUT -d value="True"
 
 if [ "$(curl -s http://${ETCD_HOST}:2379/v2/keys/mysql/leader | jq -r .node.value)" == "mysql-${HOSTNAME}" ]; then
+while  [ $(curl -s http://${ETCD_HOST}:2379/v2/keys/mysql/pcs/ | jq -r ".node.nodes[] | select(.value == \"$(printf 'True')\") | .key" | wc -l) -lt $((${MAX_NODES})) ]; do
+sleep 1
+done
 pcs cluster auth ${HOSTS[@]} -u hacluster -p ${HACLUSTER} --force
 pcs cluster setup --force --name Cluster ${HOSTS[@]}
 pcs cluster start --all
